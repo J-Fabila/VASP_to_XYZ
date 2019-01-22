c1e4b9eb728b8bcf58d782bc8a80d38337607d69
J-Fabila <46831682+J-Fabila@users.noreply.github.com>
2019-01-21 21:20:01 -0600

    Add files via upload

---
+++ XYZtoPOSCAR.sh
@@ -0,0 +1,25 @@
+N=$(head -1 $1)
+head -$(($N+2)) $1 | tail -$N |  awk '{print $1 }' | sort -u >> aux
+M=$(wc -l aux | awk '{ print $1 }')
+for ((i=1;i<$(($M+1));i++))
+do
+echo -n "$(head -$i aux | tail -1  )  " >>$2
+done
+echo " " >>$2
+for ((i=1;i<$(($M+1));i++))
+do
+echo -n "$( grep "$(head -$i aux | tail -1 )" $1 | wc -l )  ">> $2
+done
+echo "  " >> $2
+Sel=$(head -$(($N+2)) $1 | tail -$N |  awk '{print $5 }' | grep . | wc -l )
+if [ $Sel -gt 0 ]
+then
+echo "Selective dynamics " >> $2
+fi
+
+echo "Cartesian" >> $2
+rm aux
+head -$(($N+2)) $1 | tail -$N |  sort -u | awk '{print $2 "  "$3 "  " $4 }' >> aux1
+head -$(($N+2)) $1 | tail -$N |  sort -u | awk '{print  $5 "  " $6 "  " $7}' | tr '0' 'T' >> aux2 #tr '1' 'F'
+paste aux1 aux2 >> $2
+rm aux1 aux2
