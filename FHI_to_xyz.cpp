261d09767a9ec210fbd2a08f6317ca773e5c6ea4
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2020-02-27 11:15:38 -0600

    Update FHI_to_xyz.cpp

--- FHI_to_xyz.cpp
+++ FHI_to_xyz.cpp
@@ -1,8 +1,17 @@
 #include<atomicpp.h>
 Atomic_Structure est;
-int main()
+
+int main(int argc, char **argv)
 {
 est.read_fhi(argv[1]);
-est.
+if(argc<3)
+{
+  est.print_xyz("tostd");
+  system("cat tostd ; rm tostd");
+}
+else if(argc==3)
+{
+  est.print_xyz(argv[2]);
+}
 return 0;
 }
