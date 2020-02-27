4c2f39bbb77d073a717988ebba38294d9af3c870
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2020-02-27 11:21:48 -0600

    Create VASP_to_xyz.cpp

---
+++ VASP_to_xyz.cpp
@@ -0,0 +1,17 @@
+#include"atomicpp.h"
+Atomic_Structure est;
+
+int main(int argc, char **argv)
+{
+est.read_VASP(argv[1]);
+if(argc<3)
+{
+  est.print_xyz("tostd");
+  system("cat tostd ; rm tostd");
+}
+else if(argc==3)
+{
+  est.print_xyz(argv[2]);
+}
+return 0;
+}
