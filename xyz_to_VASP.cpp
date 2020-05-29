c006d27c2caa1e9c79d76103b55ff2f73994df61
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2020-05-29 18:42:59 -0500

    Update xyz_to_VASP.cpp

--- xyz_to_VASP.cpp
+++ xyz_to_VASP.cpp
@@ -3,10 +3,12 @@ Atomic_Structure est;
 
 int main(int argc, char **argv)
 {
+// void Atomic_Structure::print_VASP(string outputfile,string Titulo, float Factor, double M[3][3])
+double M[3][3]={{25.0, 0.0 , 0.0},{0.0, 25.0 , 0.0},{0.0, 0.0 , 25.0}};
 est.read_xyz(argv[1]);
 if(argc<3)
 {
-  est.print_VASP("tostd");
+  est.print_VASP("tostd","XYZ to VASP", 1.0, M);
   system("cat tostd ; rm tostd");
 }
 else if(argc==3)
